05c224166d904dabcfa168fed04bf8692801690e
danleyb2 <ndieksman@gmail.com>
2015-09-13 16:48:38 +0300

    Better ballOnStep method 	check if ball is near step and not necessarily exactly on step 	now uses the whole step WIDTH in the check

--- src/Ball.java
+++ src/Ball.java
@@ -6,13 +6,12 @@ import java.awt.*;
  */
 public class Ball extends Rectangle {
 
-    private int RADIUS=10;
+    private int RADIUS = 10;
 
 
-
-    public Ball(){
-        this.x=200;
-        this.y=200;
+    public Ball() {
+        this.x = 200;
+        this.y = 200;
 
     }
 
@@ -20,24 +19,42 @@ public class Ball extends Rectangle {
         return RADIUS;
     }
 
-    public boolean onStep(Steps step){
 
-       if (this.x<=(step.x+step.width)-this.RADIUS && this.x>=step.x && step.y-step.height==this.y){
-           if(step.isSpike){
-               System.out.println("\tGAME OVER \tBall on spike step");
-           }else {
-               //System.out.println("Ball on step");
-           }
-        return true;
-        }
-        else {
+    /**
+     * Checks if the ball is on a step
+     *
+     * @param step
+     * @return boolean
+     */
+    public boolean onStep(Steps step) {
+
+
+        if (
+            //ball not past step right end
+                this.x <= (step.x + step.width) &&
+                        //ball past step left start
+                        this.x >= step.x &&
+                        //step top equals ball bottom
+                        (step.y - step.height == this.y || step.y - step.height == this.y - 1)
+
+                ) {
+
+            if (step.isSpike) {
+                System.out.println("\tGAME OVER \tBall on spike. SCORE:" + RapidRoll.score);
+                RapidRoll.gameOver = true;
+
+            } else {
+                //System.out.println("Ball on step");
+            }
+            return true;
+        } else {
             return false;
         }
     }
 
-    public void paint(Graphics graphics){
+    public void paint(Graphics graphics) {
         graphics.setColor(Color.BLUE);
-        graphics.fillRect(this.x,this.y,RADIUS,RADIUS);
+        graphics.fillRect(this.x, this.y, RADIUS, RADIUS);
 
     }
 
