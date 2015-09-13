b792c63456b60af0abe0a2709f9b72008e1f1723
danleyb2 <ndieksman@gmail.com>
2015-09-13 16:35:28 +0300

    Add display score and a start game method
    	display score (count of passed steps) when game over
    	startGame method for faster game restart
    	various bug fixes and performance improvements
    	removed unnecessary loop from genRandomstep

--- src/RapidRoll.java
+++ src/RapidRoll.java
@@ -2,33 +2,31 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.Timer;
 
 
-public class RapidRoll extends JFrame implements ActionListener{
+public class RapidRoll extends JFrame implements ActionListener {
 
     private Timer timer;
     private Random random;
     private RenderPanel panel;
     public static RapidRoll rapidRoll;
-    public static int WIDTH=350,HEIGHT=600;
-    private int SPACE=50;
+    public static int WIDTH = 350, HEIGHT = 600;
+    public static int score;
+    public static boolean gameOver = false;
+
+    private int SPACE = 50;
 
     public static Ball ball;
-    private ArrayList<Steps> stepsArrayList=new ArrayList<Steps>();
+    private ArrayList<Steps> stepsArrayList = new ArrayList<Steps>();
 
 
-
-
-    public RapidRoll(){
+    public RapidRoll() {
         super("Rapid Roll");
-
-        panel=new RenderPanel();
-        this.setContentPane(panel);
+        panel = new RenderPanel();
+        this.add(panel);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setAlwaysOnTop(true);
         this.setSize(WIDTH, HEIGHT);
@@ -37,56 +35,61 @@ public class RapidRoll extends JFrame implements ActionListener{
         this.addKeyListener(new EventListener());
 
 
-        timer=new Timer(20,this);
-        random=new Random();
+        timer = new Timer(20, this);
+        random = new Random();
 
-        timer.start();
 
     }
 
-    private void addBar(){
-        int sz=stepsArrayList.size();
+    private void addBar() {
+        int sz = stepsArrayList.size();
         int ly;
 
-        if (sz==0){
-             ly=(Integer)0+SPACE;
-        }else{
-           ly=(Integer)((stepsArrayList.size()*SPACE)+SPACE);
+        if (sz == 0) {
+            ly = 0 + SPACE;
+        } else {
+            ly = (stepsArrayList.size() * SPACE) + SPACE;
         }
-
-        int lx=random.nextInt(WIDTH-(Steps.getWIDTH()));
-
-        stepsArrayList.add(new Steps(lx,ly,false));
+        int lx = random.nextInt(WIDTH - (Steps.getWIDTH()));
+        stepsArrayList.add(new Steps(lx, ly, false));
 
     }
 
-    private void addSpike(){
-        int sz=stepsArrayList.size();
+    private void addSpike() {
+        int sz = stepsArrayList.size();
         int ly;
 
-        if (sz==0){
-            ly=(Integer)0+SPACE;
-        }else{
-            ly=(Integer)((stepsArrayList.size()*SPACE)+SPACE);
+        if (sz == 0) {
+            ly = (Integer) 0 + SPACE;
+        } else {
+            ly = (Integer) ((stepsArrayList.size() * SPACE) + SPACE);
         }
-        int lx=random.nextInt(WIDTH-(Steps.getWIDTH()*2));
-        stepsArrayList.add(new Steps(lx,ly,true));
+        int lx = random.nextInt(WIDTH - (Steps.getWIDTH() * 2));
+        stepsArrayList.add(new Steps(lx, ly, true));
 
     }
-    private void genRandomStep(int number){
+
+    private void genRandomSteps(int number) {
 
         for (int i = 1; i <= number; i++) {
-            if (random.nextBoolean()){
+            if (random.nextBoolean()) {
                 addSpike();
-            }else {
+            } else {
                 addBar();
             }
         }
 
     }
 
-    public void repaint(Graphics g){
-        //System.out.println("painted comp");
+    private void genRandomStep() {
+        if (random.nextBoolean()) {
+            addSpike();
+        } else {
+            addBar();
+        }
+    }
+
+    public void repaint(Graphics g) {
         ball.paint(g);
         for (Steps step : stepsArrayList) {
             step.paint(g);
@@ -95,43 +98,70 @@ public class RapidRoll extends JFrame implements ActionListener{
     }
 
 
-    public static void main(String[] args){
-       rapidRoll= new RapidRoll();
-        rapidRoll.genRandomStep(20);
-        rapidRoll.ball=new Ball();
+    public static void main(String[] args) {
+        rapidRoll = new RapidRoll();
+        rapidRoll.startGame();
 
+    }
+
+    public void startGame() {
+
+        gameOver = false;
+        score = 0;
+        stepsArrayList.clear();
+        rapidRoll.genRandomSteps(12);
+        rapidRoll.ball = new Ball();
+        timer.start();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
 
-        boolean onAstep=false;
+        boolean onAstep = false;
         for (Steps step : stepsArrayList) {
-
-            if(ball.onStep(step)){
-                onAstep=true;
-                ball.y-=1;
-                step.y-=1;
-            }else {
-
-                step.y-=1;
+            if (ball.onStep(step)) {
+                ball.y -= 1;
+                onAstep = true;
             }
+            step.y -= 1;
         }
-        if(!onAstep){ ball.y++;  }
+        if (ball.y > HEIGHT || ball.y < 0) {
+            gameOver = true;
+        }
+        panel.repaint();
+        if (gameOver) {
+            timer.stop();
+            return;
+        }
+        if (!onAstep) {
+            ball.y++;
+        }
 
         for (int i = 0; i < stepsArrayList.size(); i++) {
 
-            if (stepsArrayList.get(i).y<0){
+            if (stepsArrayList.get(i).y < 0) {
                 stepsArrayList.remove(i);
+                score++;
                 //System.out.println("removed step");
-                genRandomStep(1);
+                genRandomStep();
             }
 
         }
 
-        panel.repaint();
+    }
 
+    //Getters
 
+    public Timer getTimer() {
+        return timer;
+    }
+
+    public void paintScore(Graphics g) {
+        g.setColor(Color.BLUE);
+        g.setFont(new Font("Sans", Font.BOLD, 20));
+        g.drawString("GAME OVER!!", 50, RapidRoll.HEIGHT / 2 - 40);
+        g.drawString("SCORE :" + RapidRoll.score, 60, RapidRoll.HEIGHT / 2 - 20);
+        g.drawString("Press enter to start", 70, RapidRoll.HEIGHT / 2);
     }
 }
